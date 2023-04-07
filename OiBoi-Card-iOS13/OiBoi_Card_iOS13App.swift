e155e31d9d125b52b5c2b79059f80bd796122290
oiiboii <omer@tactive.xyz>
2023-04-07 14:08:02 -0400

    Initial Commit

---
+++ OiBoi-Card-iOS13/OiBoi_Card_iOS13App.swift
@@ -0,0 +1,17 @@
+//
+//  OiBoi_Card_iOS13App.swift
+//  OiBoi-Card-iOS13
+//
+//  Created by Omer Ifrah on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct OiBoi_Card_iOS13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
