1e0d2ed766f1b35fa38277d23abc7ce76ab600a6
oiiboii <omer@tactive.xyz>
2023-04-18 21:58:20 -0400

    added qr code + animations and social media icons

--- OiBoi-Card-iOS13/ContentView.swift
+++ OiBoi-Card-iOS13/ContentView.swift
@@ -14,19 +14,26 @@ struct GradientBackground: View {
 }
 
 struct ContentView: View {
-    var body: some View {
-        ZStack {
-            GradientBackground()
+    @State private var scale: CGFloat = 1.0
 
-            VStack(spacing: 20) {
-                Image("oiboi_photo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 150)
-                    .clipShape(Circle())
-                    .overlay(Circle()
-                        .stroke(Color.white.opacity(0.5), lineWidth: 4)
-                    ).shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 4)
+     var body: some View {
+         ZStack {
+             GradientBackground()
+             
+             VStack(spacing: 20) {
+                 Image("profile_photo")
+                     .resizable()
+                     .aspectRatio(contentMode: .fit)
+                     .frame(width: 150)
+                     .clipShape(Circle())
+                     .overlay(Circle()
+                         .stroke(Color.white.opacity(0.5), lineWidth: 4)
+                     ).shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 4)
+                     .scaleEffect(scale)
+                     .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true), value: scale)
+                     .onAppear {
+                         scale = 1.1
+                     }
                 
                 Text("Omer Ifrah")
                     .font(.system(size: 40))
@@ -34,18 +41,35 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .kerning(2)
                 
-                Text("Product Engineer")
+                Text("iOS Developer")
                     .foregroundColor(.white)
                     .font(.system(size: 20))
                     .fontWeight(.medium)
+                
+                Text("Creating beautiful and functional iOS apps.")
+                    .foregroundColor(.white)
+                    .font(.system(size: 16))
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
                 
                 Divider()
                     .background(Color.white)
                     .padding(.horizontal, 40)
                     .padding(.vertical, 20)
                 
-                ContactInfoView(label: "Phone", value: "+1 (631) 946 9190")
+                ContactInfoView(label: "Phone", value: "+1 (631) 946-9190")
                 ContactInfoView(label: "Email", value: "oi38@cornell.edu")
+                
+                HStack(spacing: 30) {
+                    SocialMediaButton(iconName: "logo-twitter", url: "https://twitter.com/username")
+                    SocialMediaButton(iconName: "logo-linkedin", url: "https://www.linkedin.com/in/username")
+                    SocialMediaButton(iconName: "logo-github", url: "https://github.com/username")
+                }.padding(.top, 20)
+                
+                QRCodeView(value: "https://www.example.com/JohnDoe")
+                    .frame(width: 100, height: 100)
+                    .padding(.top, 20)
             }
             .padding(.top, 100)
             .padding(.horizontal, 20)
@@ -78,6 +102,50 @@ struct ContactInfoView: View {
     }
 }
 
+struct QRCodeView: View {
+    let value: String
+    
+    var body: some View {
+        Image(uiImage: generateQRCode(from: value))
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+    }
+    private func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        let context = CIContext()
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            
+            if let outputImage = filter.outputImage {
+                if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                    return UIImage(cgImage: cgImage)
+                }
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+}
+struct SocialMediaButton: View {
+    let iconName: String
+    let url: String
+    
+    var body: some View {
+        Button(action: {
+            if let url = URL(string: url), UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+            }
+        }) {
+            Image(systemName: iconName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 24, height: 24)
+                .foregroundColor(.white)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
